655a2d004112a8b25327d545dec083240aaa60f0
hideokamoto <hide.okamoto@digitalcube.jp>
2016-02-21 16:30:54 +0900

    Declare WooCommerce support

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'lightning_parent_theme_enqueue_styles' );
-
 function lightning_parent_theme_enqueue_styles() {
     wp_enqueue_style( 'lightning-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'lightning-woo-style',
@@ -9,4 +8,9 @@ function lightning_parent_theme_enqueue_styles() {
         array( 'lightning-style' )
     );
 
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+	add_theme_support( 'woocommerce' );
 }
